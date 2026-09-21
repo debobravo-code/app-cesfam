9fc194a830748948b66c051b69229b32c1da69d3
mik.palma <mik.palma@duocuc.cl>
2026-09-21 20:00:59 -0300

    Agrega proyecto base Spring Boot

---
+++ CesfamInterno/CesfamInterno/src/main/java/cl/duoc/CesfamInterno/CesfamInternoApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.CesfamInterno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CesfamInternoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CesfamInternoApplication.class, args);
+	}
+
+}
---
+++ CesfamInterno/CesfamInterno/src/test/java/cl/duoc/CesfamInterno/CesfamInternoApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.CesfamInterno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CesfamInternoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
