53815002bdbaa29faa86c81b404e4b2d80eb2b03
Debora Bravo <debo.bravo@duocuc.cl>
2026-09-21 23:00:52 -0300

    Agrega microservicio Presencia y Disponibilidad

---
+++ presencia-disponibilidad-service/presencia-disponibilidad-service/src/main/java/cl/duoc/presenciadisponibilidad/PresenciaDisponibilidadServiceApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.presenciadisponibilidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresenciaDisponibilidadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresenciaDisponibilidadServiceApplication.class, args);
+	}
+
+}
---
+++ presencia-disponibilidad-service/presencia-disponibilidad-service/src/test/java/cl/duoc/presenciadisponibilidad/PresenciaDisponibilidadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.presenciadisponibilidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresenciaDisponibilidadServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
