276402eefef3bf9e82044bd529ed51e7dcb4a8da
Debora Bravo <debo.bravo@duocuc.cl>
2026-09-23 20:53:50 -0300

    crear entidad Notificacion

---
+++ notificaciones-service/notificaciones-service/src/main/java/cl/duoc/notificaciones/model/Notificacion.java
@@ -0,0 +1,33 @@
+
+package cl.duoc.notificaciones.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "notificaciones")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Notificacion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long funcionarioId;
+
+    private String titulo;
+
+    private String mensaje;
+
+    private String tipo;
+
+    private String estado;
+
+    private LocalDateTime fechaCreacion;
+}
