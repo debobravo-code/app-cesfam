441c9e1a39813861bb5dc3a630b176b230f071a4
mik.palma <mik.palma@duocuc.cl>
2026-09-24 18:26:43 -0300

    crear microservicio de estacionamiento

---
+++ estacionamiento-service/estacionamiento-service/src/main/java/cl/duoc/estacionamiento_service/EstacionamientoServiceApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.estacionamiento_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstacionamientoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstacionamientoServiceApplication.class, args);
+	}
+
+}
---
+++ estacionamiento-service/estacionamiento-service/src/test/java/cl/duoc/estacionamiento_service/EstacionamientoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.estacionamiento_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstacionamientoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
